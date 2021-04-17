8dddea061e05388a2d69dbf80b3c74874b51dd76
Saberzhan19 <Saberzhax@gmail.com>
2021-04-17 06:19:28 +0600

    Изменнения в стиле css

--- home.php
+++ home.php
@@ -6,12 +6,8 @@ if(!isset($_SESSION['username'])){
 }
 
 ?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -21,8 +17,8 @@ if(!isset($_SESSION['username'])){
 
     <div class="container">
         
-    <a class="float-right" href="logout.php">LOGOUT</a>
-    <h1>Welcome <?php echo $_SESSION['username']; ?> </h1>
+    <a class="float-right" href="logout.php"> LOGOUT </a>
+    <h1> Welcome <?php echo $_SESSION['username'];?> </h1>
  
     </div>
      
